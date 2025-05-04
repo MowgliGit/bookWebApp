942168ad4403aef8ae62fa2d9e83a7e11ed15394
MowgliGit <marina.jovanova17@gmail.com>
2025-05-04 21:07:20 +0200

    some changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,8 @@ const Footer = () => {
       <div className="footerContent">
         <p className="footerLogo">📚 One Click to Your Next Book</p>
         <p className="footerText">
-          &copy; {new Date().getFullYear()} BookWeb. All rights reserved Mowgli.
+          &copy; {new Date().getFullYear()} BookWeb 📚 All rights reserved
+          Mowgli.
         </p>
       </div>
     </footer>
